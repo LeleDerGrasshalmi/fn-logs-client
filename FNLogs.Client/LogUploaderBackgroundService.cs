1a4dea0416247561ca86728ef014dcb7b3ba2333
LeleDerGrasshalmi <king-of-gosts@web.de>
2024-04-02 20:16:00 +0200

    Projektdateien hinzufügen.

---
+++ FNLogs.Client/LogUploaderBackgroundService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FNLogs.Client;
+
+public class LogUploaderBackgroundService : BackgroundService
+{
+    private readonly ILogger<LogUploaderBackgroundService> _logger;
+
+    public LogUploaderBackgroundService(ILogger<LogUploaderBackgroundService> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Background service is running at: {time}", DateTimeOffset.Now);
+
+            await Task.Delay(TimeSpan.FromMinutes(15), stoppingToken);
+        }
+    }
+}
---
+++ FNLogs.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FNLogs.Client;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        IHost host = new HostBuilder()
+            .ConfigureLogging((hostContext, loggingBuilder) =>
+            {
+                loggingBuilder.AddConsole();
+            })
+            .ConfigureServices((_, services) =>
+            {
+                services.AddHostedService<LogUploaderBackgroundService>();
+            })
+            .UseConsoleLifetime()
+            .Build();
+
+        await host.RunAsync();
+    }
+}

408a0208be26a9eb1f4006d5e621fdcc689e7787
LeleDerGrasshalmi <king-of-gosts@web.de>
2024-04-05 23:13:42 +0200

    add serilog file logging

--- FNLogs.Client/Program.cs
+++ FNLogs.Client/Program.cs
@@ -6,29 +6,32 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Serilog;
+
 namespace FNLogs.Client;
 
 internal class Program
 {
     public const string AppSettingsName = "appsettings.json";
-    public static string LocalAppSettings = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FNLogs", AppSettingsName);
+    public static string LocalAppDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FNLogs");
+    public static string LogsPath = Path.Combine(LocalAppDataPath, "Logs");
+    public static string LocalAppSettings = Path.Combine(LocalAppDataPath, AppSettingsName);
     public static string BaseAppSettings = Path.Combine(Environment.CurrentDirectory, AppSettingsName);
 
     public static async Task Main(string[] args)
     {
+        if (LogsPath is not null
+            && !Directory.Exists(LogsPath))
+        {
+            Directory.CreateDirectory(LogsPath);
+        }
+
         IHost host = new HostBuilder()
             .ConfigureAppConfiguration((configBuilder) =>
             {
 #if !DEBUG
                 if (!File.Exists(LocalAppSettings))
                 {
-                    string? dir = Path.GetDirectoryName(LocalAppSettings);
-
-                    if (dir is not null && !Directory.Exists(dir))
-                    {
-                        Directory.CreateDirectory(dir);
-                    }
-
                     File.Copy(BaseAppSettings, LocalAppSettings);
                 }
 
@@ -39,13 +42,20 @@ internal class Program
             })
             .ConfigureLogging((hostContext, loggingBuilder) =>
             {
-#if DEBUG
-                LogLevel level = LogLevel.Trace;
-#else
                 LogLevel level = LogLevel.Information;
+                LoggerConfiguration config = new LoggerConfiguration()
+                    .WriteTo.File($"{LogsPath}/fn-logs-client.log", rollingInterval: RollingInterval.Day);
+
+#if DEBUG
+                config.MinimumLevel.Verbose();
+                level = LogLevel.Trace;
+#else
+                config.MinimumLevel.Information();
 #endif
+                Log.Logger = config.CreateLogger();
 
                 loggingBuilder
+                    .AddSerilog(dispose: true)
                     .AddConsole()
                     .SetMinimumLevel(level);
             })
